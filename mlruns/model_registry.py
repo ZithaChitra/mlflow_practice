b9b712c916f5817c93a73aa7481396fb2fd85fce
zithachitra <realblessingchitra@gmail.com>
2021-03-21 19:12:22 +0000

    Additional Model Registry Methods

--- mlruns/model_registry.py
+++ mlruns/model_registry.py
@@ -60,15 +60,67 @@ model.predict(data)
 
 # Fetch the latest model version in a specific stage
 
+import mlflow.pyfunc
+
+model_name = "sk-learn-random-forest-reg-model"
+stage = "Staging"
+
+model = mlflow.pyfunc.load_model(
+	model_uri = f"models://{model_name}/{stage}"
+)
+
+model.predict(data)
 
 
 
+# Adding or updataing an MLflow Model Description
+
+from mlflow.tracking import MlflowClient
+
+client = MlflowClient()
+client.update_model_version(
+	name="sk-learn-random-forest-reg-model",
+	version=1,
+	description="This is the new description of the model"
+)
+
+
+# Staging|Archived|Production|None
+client.transiton_model_version_stage(
+	name="sk-learn-random-forest-reg-model",
+	version=3,
+	stage="Production"
+)
+
+
+# Listing and searching mlflow models
+# You can fetch a list of all registered models in the registry
+# with a simple method
+from pprint import pprint
+
+client = MlflowClient()
+for rm in client.list_registered_models():
+	pprint(rm)
+
+
+# A more efficient approach would be to search for a specific model
+# name and list it's version details using
+# search_model_versions() method
+
+client = MlflowClient()
+
+for mv in client.search_model_versions("name='sk-learn-model'"):
+	pprint(dict(mv), indent=4)
 
 
 
-
-
-
+# Archive model version 3 from production
+client = MlflowClient()
+client.transition_model_version_stage(
+	name="sk-learn-random-forest-reg-model",
+	version=3,
+	stage="Archived"
+)
 
 
 
